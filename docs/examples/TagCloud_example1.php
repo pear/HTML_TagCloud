d49e97ed7c58677b745ceb59e972a16e461dd1a0
Bastian Onken <bastianonken@php.net>
2007-11-22 12:47:03 +0000

    fixed typo in perl url
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@246725 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/TagCloud_example1.php
+++ docs/examples/TagCloud_example1.php
@@ -8,7 +8,7 @@ $tags = new HTML_TagCloud();
 // add Element
 $tags->addElement('PHP','http://www.php.net', 39, strtotime('-1 day'));
 $tags->addElement('XML','http://www.xml.org', 21, strtotime('-2 week'));
-$tags->addElement('Perl','http://www.xml.org', 15, strtotime('-1 month'));
+$tags->addElement('Perl','http://www.perl.org', 15, strtotime('-1 month'));
 $tags->addElement('PEAR','http://pear.php.net', 32, time());
 $tags->addElement('MySQL','http://www.mysql.com', 10, strtotime('-2 day'));
 $tags->addElement('PostgreSQL','http://pgsql.com', 6, strtotime('-3 week'));
