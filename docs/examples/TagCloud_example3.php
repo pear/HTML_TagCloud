90a82b9d507f49943c1238b6255a367da3c07867
Bastian Onken <bastianonken@php.net>
2007-12-15 15:56:07 +0000

    * added docblocks * changed variable names to be conform with class members * switched to UTC timezone as default * formattings (according to CS)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@248314 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/TagCloud_example3.php
+++ docs/examples/TagCloud_example3.php
@@ -1,17 +1,49 @@
 <?php
-// $Id$
+
+/**
+ * TagCloud_example3.php
+ *
+ * Generate a highly customized Tag Cloud by extending the class.
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML
+ * @package   HTML_TagCloud
+ * @author    Bastian Onken <bastian.onken@gmx.net>
+ * @copyright 2007 Bastian Onken
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/HTML_TagCloud
+ * @since     File available since Release 0.1.0
+ */
 
 require_once 'HTML/TagCloud.php';
 
 // {{{ class MyTags extends HTML_TagCloud{
+
 /**
+ * MyTags extends HTML_TagCloud
  *
- * OverRide protected class vars
+ * Showing how to override the protected class vars
  *
- * @author     Shoma Suzuki <shoma@catbot.net> 
- * @version    $Id$
+ * @category  HTML
+ * @package   HTML_TagCloud
+ * @author    Shoma Suzuki <shoma@catbot.net>
+ * @author    Bastian Onken <bastian.onken@gmx.net>
+ * @copyright 2006 Shoma Suzuki
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/HTML_TagCloud
+ * @since     Class available since Release 0.1.0
  */
-class MyTags extends HTML_TagCloud{
+class MyTags extends HTML_TagCloud
+{
     protected $epocLevel = array(
         array(
             'earliest' => array(
@@ -66,20 +98,22 @@ class MyTags extends HTML_TagCloud{
     protected $fontSizeRange = 0;
     protected $baseFontSize = 12;
 }
+
 // }}}
 
-date_default_timezone_set('Asia/Tokyo');
-$tags = new MyTags();
+date_default_timezone_set('UTC');
 
-// below same as example2
-// add Element
-$tags->addElement('PHP','http://www.php.net', 39, strtotime('-1 day'));
-$tags->addElement('XML','http://www.xml.org', 21, strtotime('-2 week'));
-$tags->addElement('Perl','http://www.xml.org', 15, strtotime('-1 month'));
-$tags->addElement('PEAR','http://pear.php.net', 32, time());
-$tags->addElement('MySQL','http://www.mysql.com', 10, strtotime('-2 day'));
-$tags->addElement('PostgreSQL','http://pgsql.com', 6, strtotime('-3 week'));
+$tags = new MyTags();
+// add Elements (same as in example1)
+$tags->addElement('PHP', 'http://www.php.net', 39, strtotime('-1 day'));
+$tags->addElement('XML', 'http://www.xml.org', 21, strtotime('-2 week'));
+$tags->addElement('Perl', 'http://www.perl.org', 15, strtotime('-1 month'));
+$tags->addElement('PEAR', 'http://pear.php.net', 32, time());
+$tags->addElement('MySQL', 'http://www.mysql.com', 10, strtotime('-2 day'));
+$tags->addElement('PostgreSQL', 'http://pgsql.com', 6, strtotime('-3 week'));
 // output HTML and CSS
 print $tags->buildALL();
+
 show_source(__FILE__);
+
 ?>
