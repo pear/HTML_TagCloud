7ebd23b526a9d33d7e2c9d6d723b0086ee5db57f
Bastian Onken <bastianonken@php.net>
2007-12-15 17:54:16 +0000

    * added docblocks * changed variable names to be conform with class members * set lang of html page to "en" and removed unused html tags / cleaned up structure * added source display like in example1 and example3 * formattings (according to CS)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@248324 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/TagCloud_example2.php
+++ docs/examples/TagCloud_example2.php
@@ -1,17 +1,44 @@
 <?php
-// $Id$
+
+/**
+ * TagCloud_example2.php
+ *
+ * Generate a Tag Cloud with non-default font sizes embedded into HTML. Also use
+ * the array function "addElements" in conjunction with "addElement" to mass set
+ * up tags.
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML
+ * @package   HTML_TagCloud
+ * @author    Bastian Onken <bastian.onken@gmx.net>
+ * @copyright 2007 Bastian Onken
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/HTML_TagCloud
+ * @since     File available since Release 0.1.0
+ */
 
 require_once 'HTML/TagCloud.php';
 
-$basefontsize = 36;
-$fontsizerange = 24;
-// Tag size range in ($basefontsize - $fontsizerange) to ($basefontsize + $fontsizerange).
-$tags = new HTML_TagCloud($basefontsize, $fontsizerange);
+date_default_timezone_set('UTC');
+
+$baseFontSize  = 36;
+$fontSizeRange = 24;
+// Tag size range in ($baseFontSize - $fontSizeRange) to
+// ($baseFontSize + $fontSizeRange).
+$tags = new HTML_TagCloud($baseFontSize, $fontSizeRange);
 $name = 'a';
 // set a item without timestamp
-foreach(range(0,15) as $i){
-    $arr[$i]['name'] = $name;
-    $arr[$i]['url']  = '#';
+foreach (range(0, 15) as $i) {
+    $arr[$i]['name']  = $name;
+    $arr[$i]['url']   = '#';
     $arr[$i]['count'] = $i;
     $name++;
 }
@@ -19,18 +46,16 @@ foreach(range(0,15) as $i){
 $tags->addElements($arr);
 $tags->addElement('H', '#', 16);
 $tags->addElement('P', '#', 18);
-
 // CSS part only
 $css = $tags->buildCSS();
 // html part only
 $taghtml = $tags->buildHTML();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html lang="ja">
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-JP">
-<meta http-equiv="Content-Script-Type" content="text/javascript">
-<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>My first Tag Cloud</title>
 <style type="text/css">
 <?php 
 print $css;
@@ -40,6 +65,7 @@ print $css;
 <body>
 <?php
 print $taghtml;
+show_source(__FILE__);
 ?>
 </body>
 </html>
