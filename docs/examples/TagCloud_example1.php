dbcebe8029f940bbf2da03ea83bf9559603705ef
Bastian Onken <bastianonken@php.net>
2007-12-15 15:54:15 +0000

    * added docblocks * switched to UTC timezone as default * formattings (according to CS)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@248313 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/TagCloud_example1.php
+++ docs/examples/TagCloud_example1.php
@@ -1,18 +1,43 @@
 <?php
-// $Id$
+
+/**
+ * TagCloud_example1.php
+ *
+ * Generate a simple Tag Cloud
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  HTML
+ * @package   HTML_TagCloud
+ * @author    Bastian Onken <bastian.onken@gmx.net>
+ * @copyright 2007 Bastian Onken
+ * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/HTML_TagCloud
+ * @since     File available since Release 0.1.0
+ */
 
 require_once 'HTML/TagCloud.php';
 
-date_default_timezone_set('Asia/Tokyo');
+date_default_timezone_set('UTC');
+
 $tags = new HTML_TagCloud();
-// add Element
-$tags->addElement('PHP','http://www.php.net', 39, strtotime('-1 day'));
-$tags->addElement('XML','http://www.xml.org', 21, strtotime('-2 week'));
-$tags->addElement('Perl','http://www.perl.org', 15, strtotime('-1 month'));
-$tags->addElement('PEAR','http://pear.php.net', 32, time());
-$tags->addElement('MySQL','http://www.mysql.com', 10, strtotime('-2 day'));
-$tags->addElement('PostgreSQL','http://pgsql.com', 6, strtotime('-3 week'));
+// add Elements
+$tags->addElement('PHP', 'http://www.php.net', 39, strtotime('-1 day'));
+$tags->addElement('XML', 'http://www.xml.org', 21, strtotime('-2 week'));
+$tags->addElement('Perl', 'http://www.perl.org', 15, strtotime('-1 month'));
+$tags->addElement('PEAR', 'http://pear.php.net', 32, time());
+$tags->addElement('MySQL', 'http://www.mysql.com', 10, strtotime('-2 day'));
+$tags->addElement('PostgreSQL', 'http://pgsql.com', 6, strtotime('-3 week'));
 // output HTML and CSS
 print $tags->buildALL();
+
 show_source(__FILE__);
+
 ?>
