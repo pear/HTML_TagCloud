e16185ec32faa48105a45926acac8fb3ebaae259
Bastian Onken <bastianonken@php.net>
2008-01-21 16:56:33 +0000

    added more tests
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@251073 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/TagCloudTest.php
+++ tests/TagCloudTest.php
@@ -74,9 +74,119 @@ class TagCloudTest extends PHPUnit_Framework_TestCase
      */
     function testAddElement()
     {
-        $this->HTML_TagCloud->addElement('Tag');
+        $this->HTML_TagCloud->addElement('tag0');
         $result = $this->HTML_TagCloud->buildHTML();
-        $this->assertTrue(!empty($result));
+        $this->assertFalse(empty($result));
+    }
+
+    /**
+     * test the addElements function
+     *
+     * @return void
+     */
+    function testAddElements() {
+        $tags = array(
+            array('name' => 'tag1'),
+            array('name' => 'tag2', 'url' => 'http://example.org'),
+            array('name' => 'tag3', 'url' => 'http://example.org', 'count' => 3),
+            array('name' => 'tag4', 'url' => 'http://example.org', 'count' => 4, 'timestamp' => time()),
+        );
+        $this->HTML_TagCloud->addElements($tags);
+        $result = $this->HTML_TagCloud->buildHTML();
+        $this->assertFalse(empty($result));
+    }
+
+    /**
+     * test the buildCSS function
+     *
+     * @return void
+     */
+    function testBuildCSS() {
+        $expected = <<<EOT
+a.earliest:link {text-decoration: none; color: #cccccc;}
+a.earliest:visited {text-decoration: none; color: #cccccc;}
+a.earliest:hover {text-decoration: none; color: #cccccc;}
+a.earliest:active {text-decoration: none; color: #cccccc;}
+a.earlier:link {text-decoration: none; color: #9999cc;}
+a.earlier:visited {text-decoration: none; color: #9999cc;}
+a.earlier:hover {text-decoration: none; color: #9999cc;}
+a.earlier:active {text-decoration: none; color: #9999cc;}
+a.later:link {text-decoration: none; color: #9999ff;}
+a.later:visited {text-decoration: none; color: #9999ff;}
+a.later:hover {text-decoration: none; color: #9999ff;}
+a.later:active {text-decoration: none; color: #9999ff;}
+a.latest:link {text-decoration: none; color: #0000ff;}
+a.latest:visited {text-decoration: none; color: #0000ff;}
+a.latest:hover {text-decoration: none; color: #0000ff;}
+a.latest:active {text-decoration: none; color: #0000ff;}
+
+EOT;
+        $this->assertEquals($expected, $this->HTML_TagCloud->buildCSS(), 'my message');
+    }
+
+    /**
+     * test the buildHTML function
+     *
+     * @return void
+     */
+    function testBuildHTML() {
+        $expected = <<<EOT
+<div class="tagcloud"><a href="" style="font-size: 12px;" class="latest">tag0</a>&nbsp;
+<a href="" style="font-size: 12px;" class="latest">tag1</a>&nbsp;
+<a href="http://example.org" style="font-size: 12px;" class="latest">tag2</a>&nbsp;
+<a href="http://example.org" style="font-size: 24px;" class="latest">tag3</a>&nbsp;
+<a href="http://example.org" style="font-size: 36px;" class="earliest">tag4</a>&nbsp;
+</div>
+
+EOT;
+        $this->assertEquals($expected, $this->HTML_TagCloud->buildHTML());
+    }
+
+    /**
+     * test the buildAll function
+     *
+     * @return void
+     */
+    function testBuildAll() {
+        $expected = <<<EOT
+<style type="text/css">
+a.earliest:link {text-decoration: none; color: #cccccc;}
+a.earliest:visited {text-decoration: none; color: #cccccc;}
+a.earliest:hover {text-decoration: none; color: #cccccc;}
+a.earliest:active {text-decoration: none; color: #cccccc;}
+a.earlier:link {text-decoration: none; color: #9999cc;}
+a.earlier:visited {text-decoration: none; color: #9999cc;}
+a.earlier:hover {text-decoration: none; color: #9999cc;}
+a.earlier:active {text-decoration: none; color: #9999cc;}
+a.later:link {text-decoration: none; color: #9999ff;}
+a.later:visited {text-decoration: none; color: #9999ff;}
+a.later:hover {text-decoration: none; color: #9999ff;}
+a.later:active {text-decoration: none; color: #9999ff;}
+a.latest:link {text-decoration: none; color: #0000ff;}
+a.latest:visited {text-decoration: none; color: #0000ff;}
+a.latest:hover {text-decoration: none; color: #0000ff;}
+a.latest:active {text-decoration: none; color: #0000ff;}
+</style>
+<div class="tagcloud"><a href="" style="font-size: 12px;" class="latest">tag0</a>&nbsp;
+<a href="" style="font-size: 12px;" class="latest">tag1</a>&nbsp;
+<a href="http://example.org" style="font-size: 12px;" class="latest">tag2</a>&nbsp;
+<a href="http://example.org" style="font-size: 24px;" class="latest">tag3</a>&nbsp;
+<a href="http://example.org" style="font-size: 36px;" class="earliest">tag4</a>&nbsp;
+</div>
+
+EOT;
+        $this->assertEquals($expected, $this->HTML_TagCloud->buildAll());
+    }
+
+    /**
+     * test the clearElements function
+     *
+     * @return void
+     */
+    function testClearElements() {
+        $this->HTML_TagCloud->clearElements();
+        $result = $this->HTML_TagCloud->buildHTML();
+        $this->assertTrue(empty($result));
     }
 
 }
