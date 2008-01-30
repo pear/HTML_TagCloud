734483a605581ad42d349495076ef8c6edfbe374
Bastian Onken <bastianonken@php.net>
2008-01-30 18:34:25 +0000

    * simplified example * added documentation
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_TagCloud/trunk@251822 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/TagCloud_example4.php
+++ docs/examples/TagCloud_example4.php
@@ -21,153 +21,79 @@
  * @version   CVS: $Id$
  * @link      http://pear.php.net/package/HTML_TagCloud
  * @since     File available since Release 0.1.0
- * @todo      fix issue with setting up basefonsize and fontsizerange on extending class (doesn't get recognized when instance isn't constructed with these values; so they are useless in most of the cases)
  */
 
 require_once 'HTML/TagCloud.php';
 
 /**
- * MyTagsAdvanced extends HTML_TagCloud
+ * MyTagsDynamicColors extends HTML_TagCloud
  *
  * Showing how to override the protected class vars
  *
  * @category  HTML
  * @package   HTML_TagCloud
- * @author    Shoma Suzuki <shoma@catbot.net>
  * @author    Bastian Onken <bastian.onken@gmx.net>
  * @copyright 2008 Bastian Onken
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
  * @version   Release: @package_version@
  * @link      http://pear.php.net/package/HTML_TagCloud
- * @since     Class available since Release 0.1.0
- */
-class MyTagsAdvanced extends HTML_TagCloud
-{
-    protected $epocLevel = array(
-        array(
-            'earliest' => array(
-                'link'    => 'D3D3D3',
-                'visited' => 'D3D3D3',
-                'hover'   => 'D3D3D3',
-                'active'  => 'D3D3D3',
-            ),
-        ),
-        array(
-            'earlier' => array(
-                'link'    => 'AFB8CC',
-                'visited' => 'AFB8CC',
-                'hover'   => 'AFB8CC',
-                'active'  => 'AFB8CC',
-            ),
-        ),
-        array(
-            'later' => array(
-                'link'    => '5F72AA',
-                'visited' => '5F72AA',
-                'hover'   => '5F72AA',
-                'active'  => '5F72AA',
-            ),
-        ),
-        array(
-            'latest' => array(
-                'link'    => '000097',
-                'visited' => '000097',
-                'hover'   => '000097',
-                'active'  => '000097',
-            ),
-        ),
-    );
-    protected $sizeSuffix = '%';
-    protected function _createHTMLTag($tag, $type, $fontSize)
-    {
-        return '<a href="'. $tag['url'] . '" style="font-size: '.
-               $fontSize . $this->sizeSuffix . ';" class="tagElement '.  $type .'">' .
-               htmlspecialchars($tag['name']) . '</a> '. "\n";
-    }
-}
-
-/**
- * MyTagsAdvanced extends HTML_TagCloud
- *
- * Showing how to override the protected class vars
- *
- * @category  HTML
- * @package   HTML_TagCloud
- * @author    Shoma Suzuki <shoma@catbot.net>
- * @author    Bastian Onken <bastian.onken@gmx.net>
- * @copyright 2008 Bastian Onken
- * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version   Release: @package_version@
- * @link      http://pear.php.net/package/HTML_TagCloud
- * @since     Class available since Release 0.1.0
+ * @since     Class available since Release 0.1.3
  */
 class MyTagsDynamicColors extends HTML_TagCloud
 {
     protected $sizeSuffix = '%';
-    public function __construct($baseFontSize = 24, $fontSizeRange = 12, $epocLevel)
+
+    /**
+     * create a Element of HTML part
+     *
+     * @param array  $tag      tagname
+     * @param string $type     css class of time line param
+     * @param float  $fontSize size of the font for this tag
+     *
+     * @return string a Element of Tag HTML
+     *
+     * @access protected
+     * @since Method available since Release 0.1.3
+     */
+    protected function createHTMLTag($tag, $type, $fontSize)
     {
-        $this->baseFontSize  = $baseFontSize;
-        $this->fontSizeRange = $fontSizeRange;
-        if ($this->baseFontSize - $this->fontSizeRange > 0) {
-            $this->minFontSize = $this->baseFontSize - $this->fontSizeRange;
-        } else {
-            $this->minFontSize = 0;
-        }
-        $this->maxFontSize = $this->baseFontSize + $this->fontSizeRange;
-        $this->epocLevel = $epocLevel;
-    }
-    protected function _createHTMLTag($tag, $type, $fontSize)
-    {
-        return '<a href="'. $tag['url'] . '" style="font-size: '.
-               $fontSize . $this->sizeSuffix . ';" class="tagElement '.  $type .'">' .
-               htmlspecialchars($tag['name']) . '</a> '. "\n";
+        return '<a href="'.$tag['url'].'" style="font-size: '.
+               $fontSize.$this->sizeSuffix.';" class="tagElement '.$type.'">'.
+               htmlspecialchars($tag['name']).'</a> '."\n";
     }
 }
-
-require_once 'Image/Color.php';
-
-$imageColor = new Image_Color();
-$imageColor->setWebSafe(false);
-$imageColor->setColors('000090', 'FFFFFF');
-foreach ($imageColor->getRange(4) as $key => $color) {
-    echo('<span style="color:'.$color.';">text</span>');
-    $epocLevel[]['epocLevel'.$key] = array(
-        'link'    => $color,
-        'visited' => $color
-    );
-}
-$epocLevel = array_reverse($epocLevel);
 
 date_default_timezone_set('UTC');
 
 $baseFontSize  = 140;
 $fontSizeRange = 50;
-// Tag size range in ($baseFontSize - $fontSizeRange) to
+$dummyURL      = 'http://example.org';
+// Tag font size range will result in ($baseFontSize - $fontSizeRange) to
 // ($baseFontSize + $fontSizeRange).
-$tags = new MyTagsDynamicColors($baseFontSize, $fontSizeRange, $epocLevel);
+$tags = new MyTagsDynamicColors($baseFontSize, $fontSizeRange, '000090', 'FFFFFF', 6);
 // every element has a high count
-$tags->addElement('oneYearOld(38)', 'http://example.org', 38, strtotime('-1 year'));
-$tags->addElement('halfAYearOld(34)', 'http://example.org', 34, strtotime('-6 month'));
-$tags->addElement('threeMonthOld(33)', 'http://example.org', 33, strtotime('-3 month'));
-$tags->addElement('oneMonthOld(36)', 'http://example.org', 36, strtotime('-1 month'));
-$tags->addElement('oneWeekOld(37)', 'http://example.org', 37, strtotime('-1 week'));
-$tags->addElement('now(35)', 'http://example.org', 35, strtotime('now'));
+$tags->addElement('oneYearOld(38)', $dummyURL, 38, strtotime('-1 year'));
+$tags->addElement('halfAYearOld(34)', $dummyURL, 34, strtotime('-6 month'));
+$tags->addElement('threeMonthOld(33)', $dummyURL, 33, strtotime('-3 month'));
+$tags->addElement('oneMonthOld(36)', $dummyURL, 36, strtotime('-1 month'));
+$tags->addElement('oneWeekOld(37)', $dummyURL, 37, strtotime('-1 week'));
+$tags->addElement('now(35)', $dummyURL, 35, strtotime('now'));
 // same elements, but medium count
-$tags->addElement('oneYearOld(18)', 'http://example.org', 18, strtotime('-1 year'));
-$tags->addElement('halfAYearOld(14)', 'http://example.org', 14, strtotime('-6 month'));
-$tags->addElement('threeMonthOld(13)', 'http://example.org', 13, strtotime('-3 month'));
-$tags->addElement('oneMonthOld(16)', 'http://example.org', 16, strtotime('-1 month'));
-$tags->addElement('oneWeekOld(17)', 'http://example.org', 17, strtotime('-1 week'));
-$tags->addElement('now(15)', 'http://example.org', 15, strtotime('now'));
+$tags->addElement('oneYearOld(18)', $dummyURL, 18, strtotime('-1 year'));
+$tags->addElement('halfAYearOld(14)', $dummyURL, 14, strtotime('-6 month'));
+$tags->addElement('threeMonthOld(13)', $dummyURL, 13, strtotime('-3 month'));
+$tags->addElement('oneMonthOld(16)', $dummyURL, 16, strtotime('-1 month'));
+$tags->addElement('oneWeekOld(17)', $dummyURL, 17, strtotime('-1 week'));
+$tags->addElement('now(15)', $dummyURL, 15, strtotime('now'));
 // same elements, but low count
-$tags->addElement('oneYearOld(6)', 'http://example.org', 6, strtotime('-1 year'));
-$tags->addElement('halfAYearOld(2)', 'http://example.org', 2, strtotime('-6 month'));
-$tags->addElement('threeMonthOld(1)', 'http://example.org', 1, strtotime('-3 month'));
-$tags->addElement('oneMonthOld(4)', 'http://example.org', 4, strtotime('-1 month'));
-$tags->addElement('oneWeekOld(5)', 'http://example.org', 5, strtotime('-1 week'));
-$tags->addElement('now(3)', 'http://example.org', 3, strtotime('now'));
+$tags->addElement('oneYearOld(6)', $dummyURL, 6, strtotime('-1 year'));
+$tags->addElement('halfAYearOld(2)', $dummyURL, 2, strtotime('-6 month'));
+$tags->addElement('threeMonthOld(1)', $dummyURL, 1, strtotime('-3 month'));
+$tags->addElement('oneMonthOld(4)', $dummyURL, 4, strtotime('-1 month'));
+$tags->addElement('oneWeekOld(5)', $dummyURL, 5, strtotime('-1 week'));
+$tags->addElement('now(3)', $dummyURL, 3, strtotime('now'));
 
-$tags2 = new MyTagsDynamicColors($baseFontSize, $fontSizeRange, $epocLevel);
+$tags2 = new MyTagsDynamicColors($baseFontSize, $fontSizeRange, '000090', 'FFFFFF', 1);
 // set up some tags
 $tagFixtures = array(
     'blogs',
@@ -192,17 +118,29 @@ $timeFixtures = array(
     '-1 day'
 );
 // create randomized tags
-foreach( $tagFixtures as $tag ) {
+foreach ($tagFixtures as $tag) {
     // set up how many occurrences this tag has
     $numberOfOccurrences = rand(1, 50);
     // randomize through the time fixtures and set up the oldness of this tag
     $time = $timeFixtures[rand(0, count($timeFixtures)-1)];
     // finally add it to the cloud, to see how the time and count values are
     //  interpreted we add them to the tagname to see their current value
-    $tags2->addElement($tag/*.'('.$numberOfOccurrences.','.str_replace(' ', '', $time).')'*/, 'http://example.org', $numberOfOccurrences, strtotime($time));
+    //$tag = $tag.'('.$numberOfOccurrences.','.str_replace(' ', '', $time).')';
+    $tags2->addElement($tag, $dummyURL, $numberOfOccurrences,
+                       strtotime($time));
 }
 
 $tagCloudCSS = <<<EOD
+
+EOD;
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>My customized Tag Cloud</title>
+<style type="text/css">
 .tagcloud {
     font-family: 'lucida grande',trebuchet,'trebuchet ms',verdana,arial,helvetica,sans-serif;
     line-height: 1.8em;
@@ -237,23 +175,23 @@ $tagCloudCSS = <<<EOD
     position: relative;
     vertical-align: middle;
 }
-EOD;
-
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>My customized Tag Cloud</title>
-<style type="text/css">
 <?php
-print $tagCloudCSS."\n".$tags->buildCSS();
+print $tags->buildCSS();
+print $tags2->buildCSS();
 ?>
 </style>
 </head>
 <body>
+<p>First box shows how the timeline information is expressed in detail. There is
+a static tag cloud of which tags have been named according to their time value:
+</p>
+<?php print $tags->buildHTML(); ?>
+<p>Second box shows a randomized tag cloud, hit reload to mix them up. Note: At
+this tag cloud there is no timeline information displayed (allthough there is
+timeline information set up). Here's how you deactivate it: Set the number of
+epocLevels at your own extended class to one or set the threshold of the
+to-be-generated epocLevel to '1' at constructor time:</p>
 <?php
-print $tags->buildHTML();
 print $tags2->buildHTML();
 show_source(__FILE__);
 ?>
